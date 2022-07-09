1cb76e0928fc9108a80402742cccfced3ae771f9
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-07-09 21:54:46 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,33 @@
+/*function modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passed
+  callback();
+}
+
+var arr = [1, 2, 3, 4, 5];
+
+modifyArray(arr, function() {
+  console.log("array has been modified", arr);
+});*/
+
+{
+  num = 5;
+}
+console.log(num);
+
+/*var a = 1;
+var b = 2;
+
+if (a === 1) {
+  var a = 11; // the scope is global
+  let b = 22; // the scope is inside the if-block
+
+  console.log(a);  // 11
+  console.log(b);  // 22
+}
+
+console.log(a); // 11
+console.log(b); // 2
+
+*/
